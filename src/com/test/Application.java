4c15e04b7bdc26851e5e5edf624421bf78dba5e0
전효빈 <@192.168.200.146>
2023-01-02 21:37:24 +0900

    Test commit

---
+++ src/com/test/Application.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test");
+		System.out.println("Algorithm");
+	}
+}
